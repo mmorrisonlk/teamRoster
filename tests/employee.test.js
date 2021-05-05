927df0af00bb45622f33727b2b75bbf7e96e5da8
mmorrisonlk <mmorrisonlk@gmail.com>
2021-05-05 00:10:30 -0500

    Employee tests set up and somewhat working

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -3,7 +3,22 @@ const Employee = require("../lib/employee");
 
 describe("Employee class", () => {
     it("Get name should return the name of the employee", () => {
-        const kenobi = new Employee("Kenobi")
-        expect(kenobi.getName(1)).toBe("Obi");
+        const kenobi = new Employee("Kenobi", 1, "wan@gmail.com")
+        expect(kenobi.getName()).toBe("Kenobi");
+    });
+    
+    it("Get ID should return the ID of the employee", () => {
+        const kenobi = new Employee("Kenobi", 1, "wan@gmail.com")
+        expect(kenobi.getId(1)).toBe(1);
+    });
+
+    it("Get email should return the email of the employee", () => {
+        const kenobi = new Employee("Kenobi", 1, "wan@gmail.com")
+        expect(kenobi.getEmail()).toBe("wan@gmail.com");
+    });
+
+    it("Get role should return the role of the employee", () => {
+        const kenobi = new Employee("Kenobi", 1, "wan@gmail.com")
+        expect(kenobi.getRole()).toBe("Employee");
     });
 });
