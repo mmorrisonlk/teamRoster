7013294c3879941676fb70fe3a343b39363cd7c6
mmorrisonlk <mmorrisonlk@gmail.com>
2021-05-06 21:45:00 -0500

    Got intern and manager tests up and running

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,15 @@
+const { describe, it } = require("@jest/globals");
 const Employee = require("../lib/employee");
+const Intern = require("../lib/intern");
+
+describe("Intern class", () => {
+    it("Get school should return the shcool of the Intern", () => {
+        const kenobi = new Intern("dummythiccU")
+        expect(kenobi.getSchool()).toBe("dummythiccU")
+    });
+
+    it("Get role should return Intern", () => {
+        const kenobi = new Intern()
+        expect(kenobi.getRole()).toBe("Intern");
+    });
+});
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,10 @@
+const { describe, it } = require("@jest/globals");
 const Employee = require("../lib/employee");
+const Manager = require("../lib/Manager");
+
+describe("Manager class", () => {
+    it("Get role should return Manager", () => {
+        const kenobi = new Manager()
+        expect(kenobi.getRole()).toBe("Manager");
+    });
+});
