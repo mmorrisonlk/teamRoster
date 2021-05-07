d1da7cee4867be35a81ff8722f101c90a15212d4
mmorrisonlk <mmorrisonlk@gmail.com>
2021-05-06 21:37:12 -0500

    Got engineer tests up and running

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,15 @@
+const { describe, it } = require("@jest/globals");
 const Employee = require("../lib/employee");
+const Engineer = require("../lib/engineer");
+
+describe("Engineer class", () => {
+    it("Get github should return the github of the engineer", () => {
+        const kenobi = new Engineer("yeet.git")
+        expect(kenobi.getGithub()).toBe("yeet.git")
+    });
+
+    it("Get role should return engineer", () => {
+        const kenobi = new Engineer()
+        expect(kenobi.getRole()).toBe("Engineer");
+    });
+});
